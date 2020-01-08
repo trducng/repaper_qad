85af3e9cec7e7cc4faddf82f14bf8e178420d84c
Duc Nguyen <trungduc1992@gmail.com>
2020-01-08 10:28:04 +0000

    Enhance training and inference
    - style.py: Add normalization within range of 0 and 1
    - run.py:
     + Add normalization within range of 0 and 1
     + Add style weight

--- style_transfer.pytorch/run.py
+++ style_transfer.pytorch/run.py
@@ -4,6 +4,7 @@ from pathlib import Path
 
 import cv2
 import fire
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -35,7 +36,22 @@ def normalize_imagenet_image(image, mean, std):
     return (image - mean) / std
 
 
-def train(style_image_path, image_folder, output_folder, filename, n_epochs, gpu):
+def train(style_image_path, image_folder, output_folder, filename, n_epochs, gpu, seed, load_path):
+    """Perform the training phase"""
+    ckpt = None
+    if isinstance(load_path, str):
+        ckpt = torch.load(load_path)
+        print(f'Load from {load_path}')
+
+    if ckpt:
+        seed = ckpt.get('seed', int(np.random.random() * 1e9))
+    elif not isinstance(seed, int):
+        seed = int(np.random.random() * 1e9)
+
+    print(f'Using seed {seed}')
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
     image_manipulation = transforms.Compose(
         [transforms.Resize(256), transforms.CenterCrop(256), transforms.ToTensor()]
     )
@@ -46,8 +62,11 @@ def train(style_image_path, image_folder, output_folder, filename, n_epochs, gpu
     )
 
     model_loss = VGGFeatures(pretrained=True, requires_grad=False)
-    # transform_network = UNet(pretrained=False)
-    transform_network = TransformerNet()
+    transform_network = UNet(pretrained=False)
+    # transform_network = TransformerNet()
+    if ckpt:
+        transform_network.load_state_dict(ckpt['model'])
+
     if gpu:
         model_loss = model_loss.cuda()
         transform_network = transform_network.cuda()
@@ -56,11 +75,13 @@ def train(style_image_path, image_folder, output_folder, filename, n_epochs, gpu
     content_criterion = nn.MSELoss()
 
     optimizer = optim.Adam(transform_network.parameters(), 1e-3)
+    if ckpt:
+        optimizer.load_state_dict(ckpt['optimizer'])
 
     mean = torch.FloatTensor([0.485, 0.456, 0.406]).view(-1, 1, 1)
     std = torch.FloatTensor([0.229, 0.224, 0.225]).view(-1, 1, 1)
     ys = cv2.cvtColor(cv2.imread(style_image_path), cv2.COLOR_BGR2RGB)
-    ys = torch.FloatTensor(ys).permute(2, 0, 1).unsqueeze(0)
+    ys = torch.FloatTensor(ys).permute(2, 0, 1).unsqueeze(0) / 255      # NOTE: add / 255
     ys = torch.cat([ys] * 2, dim=0)
     if gpu:
         ys = ys.cuda()
@@ -72,16 +93,21 @@ def train(style_image_path, image_folder, output_folder, filename, n_epochs, gpu
     gram_ys = [gram_matrix(each) for each in pred_ys]
 
     epoch = 0
+    if ckpt:
+        epoch = ckpt['epoch']
     while True:
         if epoch >= n_epochs:
             break
 
         total_loss = 0.0
+        total_content_loss = 0.0
+        total_style_loss = 0.0
         current_time = int(time.time())
         for idx, (x, _) in enumerate(dataloader):
             if gpu:
                 x = x.cuda()
             yhat = transform_network(x)
+            # yhat = (yhat - yhat.min()) / (yhat.max() - yhat.min())      # normalize to range 0 - 1
             yhat_transform = normalize_imagenet_image(yhat, mean, std)
             x_transform = normalize_imagenet_image(x, mean, std)
 
@@ -94,23 +120,28 @@ def train(style_image_path, image_folder, output_folder, filename, n_epochs, gpu
             for each_yhat, each_ys in zip(gram_yhat, gram_ys):
                 style_loss += style_criterion(each_yhat, each_ys)
 
-            loss = content_loss + style_loss
+            loss = 1e5 * content_loss + 1e10 * style_loss
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
             total_loss += loss.item()
+            total_content_loss += content_loss.item()
+            total_style_loss += style_loss.item()
             if idx % 1000 == 0:
                 torch.save(
                     {
                         "epoch": epoch,
                         "model": transform_network.state_dict(),
                         "optimizer": optimizer.state_dict(),
+                        "seed": seed,
                     },
                     Path(output_folder, "{}.{:04d}".format(filename, epoch)),
                 )
                 print(
-                    f"[{idx}|{len(dataset)}] Loss {total_loss / (idx + 1)} "
+                    f"[{idx}|{len(dataset)/2}] Loss {total_loss / (idx + 1)} "
+                    f"(style {total_style_loss / (idx + 1)} "
+                    f"content {total_content_loss / (idx + 1)}) "
                     f"Take {int(time.time()) - current_time} seconds"
                 )
                 current_time = int(time.time())
@@ -120,6 +151,7 @@ def train(style_image_path, image_folder, output_folder, filename, n_epochs, gpu
                 "epoch": epoch,
                 "model": transform_network.state_dict(),
                 "optimizer": optimizer.state_dict(),
+                "seed": seed,
             },
             Path(output_folder, "{}.{:04d}".format(filename, epoch)),
         )
@@ -135,8 +167,10 @@ class CLI:
         filename="model.pt",
         n_epochs=5,
         gpu=True,
+        seed=None,
+        load_path=None,
     ):
-        train(style_image_path, image_folder, output_folder, filename, n_epochs, gpu)
+        train(style_image_path, image_folder, output_folder, filename, n_epochs, gpu, seed, load_path)
 
 
 if __name__ == "__main__":
--- style_transfer.pytorch/style.py
+++ style_transfer.pytorch/style.py
@@ -5,11 +5,12 @@ import numpy as np
 import torch
 
 from models import UNet
+from transformer_net import TransformerNet
 
 
-MODEL_PATH = 'output/model.pt'
-INPUT_IMAGE_PATH = 'datasets/content/sample.jpg'
-OUTPUT_IMAGE_PATH = 'datasets/output.png'
+MODEL_PATH = '/data2/information-decomposition/john/logs/style/unet_1e5_1e10.pt.0003'
+INPUT_IMAGE_PATH = '/data2/information-decomposition/john/datasets/content/sample.jpg'
+OUTPUT_IMAGE_PATH = '/home/john/output6.png'
 GPU = True
 
 
@@ -19,6 +20,7 @@ else:
     chpt = torch.load(MODEL_PATH, map_location='cpu')
 
 model = UNet()
+# model = TransformerNet()
 model.load_state_dict(chpt['model'])
 model.eval()
 
@@ -31,6 +33,10 @@ if GPU:
     image = image.cuda()
 
 pred = model(image)
+import pdb; pdb.set_trace()
+# output_image = pred.cpu().squeeze().permute(1, 2, 0).data.numpy()       # NOTE: pred might not in range 0-1
+# output_image = (output_image - output_image.min()) / (output_image.max() - output_image.min())
+# output_image = (output_image * 255).astype(np.uint8)
 output_image = (pred.cpu().squeeze().permute(1, 2, 0).data.numpy() * 255).astype(np.uint8)
 
 cv2.imwrite(OUTPUT_IMAGE_PATH, cv2.cvtColor(output_image, cv2.COLOR_RGB2BGR))
