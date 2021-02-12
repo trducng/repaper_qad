aa977db2531df603dfb4853a2802e38773f85978
johntd54 <trungduc1992@gmail.com>
2021-02-12 10:50:33 +0000

    Perform preliminary training

--- vision_transformer.pytorch/data.py
+++ vision_transformer.pytorch/data.py
@@ -4,19 +4,19 @@ from torchvision import transforms
 
 
 imagenet_train_transform = transforms.Compose([
-    transforms.ToTensor(),
     transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0),
     transforms.RandomHorizontalFlip(p=0.5),
     transforms.RandomPerspective(),
     transforms.Resize(256),
     transforms.CenterCrop(224),
+    transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
 imagenet_eval_transform = transforms.Compose([
-    transforms.ToTensor(),
     transforms.Resize(256),
     transforms.CenterCrop(224),
+    transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
--- vision_transformer.pytorch/train.py
+++ vision_transformer.pytorch/train.py
@@ -1,3 +1,6 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '3'
+
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -7,9 +10,9 @@ from torchvision import datasets
 from data import imagenet_train_transform, imagenet_eval_transform
 from model import ViT
 
-TRAIN_BATCH_SIZE = 4
+TRAIN_BATCH_SIZE = 256
 
-IMAGENET_TRAIN = '/home/john/datasets/imagenet/object_localization/train'
+IMAGENET_TRAIN = '/home/john/john/data/train'
 IMAGENET_VAL = '/home/john/datasets/imagenet/object_localization/val'
 IMAGENET_TEST = '/home/john/datasets/imagenet/object_localization/test'
 
@@ -17,24 +20,36 @@ IMAGENET_TEST = '/home/john/datasets/imagenet/object_localization/test'
 def main():
 
     trainset = datasets.ImageFolder(root=IMAGENET_TRAIN, transform=imagenet_train_transform)
-    trainloader = DataLoader(
-        trainset, batch_size=TRAIN_BATCH_SIZE, shuffle=True, num_workers=8)
 
     model = ViT(image_size=256, patch_size=32, num_classes=1000, dim=768, depth=12,
                 heads=12, mlp_dim=3072)
     model = model.cuda()
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=0.003)
+    optimizer = optim.Adam(model.parameters(), lr=0.003, weight_decay=0.3)
+    # scheduler = optim.lr_scheduler.CosineAnnealingLR(
+    #     optimizer, T_max=5, eta_min=0.003)
 
+    epoch = 0
     model.train()
-    for input_, label in trainloader:
-        input_, label = input_.cuda(), label.cuda()
-        output = model(input_)
-        loss = criterion(output, label)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        print(f'Loss: {loss.item()}')
+    while True:
+        trainloader = DataLoader(
+            trainset, batch_size=TRAIN_BATCH_SIZE, shuffle=True, num_workers=16)
+        for idx, (input_, label) in enumerate(trainloader):
+            input_, label = input_.cuda(), label.cuda()
+            output = model(input_)
+            loss = criterion(output, label)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            if idx % 100 == 0:
+                print(f'Loss - {epoch}: {loss.item()}')
+                state_dict = {
+                    'model': model.state_dict(),
+                    'optimizer': optimizer.state_dict()
+                }
+                torch.save(state_dict, f'logs/test_{epoch}.pth')
+        epoch += 1
+        # scheduler.step(epoch)
 
 
 if __name__ == '__main__':
